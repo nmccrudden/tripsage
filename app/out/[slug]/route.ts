4bf1fe35253153d8e635fb938c0cc52b95054aa4
Niall McCrudden <nmccrudden@hotmail.com>
2026-02-13 21:28:17 +0000

    Fix affiliate redirects

--- app/out/[slug]/route.ts
+++ app/out/[slug]/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 
 const LINKS: Record<string, string> = {
   "one-world": "https://www.getyourguide.com/new-york-city-l59/nyc-one-world-observatory-skip-the-line-ticket-t552373/?partner_id=UXBBWLN&utm_medium=online_publisher",
-  vanderbilt: "https://www.getyourguide.com/new-york-city-l59/summit-one-vanderbilt-t411587/?partner_id=UXBBWLN&utm_medium=online_publisher",
-  empire: "https://www.getyourguide.com/new-york-city-l59/empire-state-building-t1017/?partner_id=UXBBWLN&utm_medium=online_publisher",
+  "vanderbilt": "https://www.getyourguide.com/new-york-city-l59/summit-one-vanderbilt-t411587/?partner_id=UXBBWLN&utm_medium=online_publisher",
+  "empire": "https://www.getyourguide.com/new-york-city-l59/empire-state-building-t1017/?partner_id=UXBBWLN&utm_medium=online_publisher",
   "top-of-the-rock": "https://www.getyourguide.com/new-york-city-l59/top-of-the-rock-t1366/?partner_id=UXBBWLN&utm_medium=online_publisher",
   "statue-of-liberty": "https://www.getyourguide.com/new-york-city-l59/statue-of-liberty-ellis-island-t154/?partner_id=UXBBWLN&utm_medium=online_publisher",
 };
@@ -15,8 +15,8 @@ export async function GET(
   const url = LINKS[params.slug];
 
   if (!url) {
-    return NextResponse.redirect(new URL("/", "https://tripsage.app"));
+    return NextResponse.redirect(new URL("https://tripsage.app"));
   }
 
-  return NextResponse.redirect(new URL(url));
+  return NextResponse.redirect(url);
 }
